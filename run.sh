3c43dc1dbcc55d01b05199f1e24e9536ed05cb98
shredder67 <seriy.karp2@gmail.com>
2023-05-10 19:32:43 +0000

    cleaned up readme and dependencies

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# if [ ! -d "env" ]; then
-#     echo "Creating virtual environment and installing dependencies..."
-#     python3 -m venv env
-#     source env/bin/activate
-#     pip install -r requirements.txt
-# else
-#     source env/bin/activate # all dependencies assumed to be installed
-# fi
+if [ ! -d "env" ]; then
+    echo "Creating virtual environment and installing dependencies..."
+    python3 -m venv env
+    source env/bin/activate
+    pip install -r requirements.txt
+else
+    source env/bin/activate # all dependencies assumed to be installed
+fi
 
-# echo "Local environment set up!"
+echo "Local environment set up!"
 
 # check for data directory, if not present, download and unpack movielens dataset
 
@@ -30,12 +30,5 @@ if [ ! -d "output" ]; then
     mkdir output/model
 fi
 
-# some evironment variables for reproducibility
-export RANDOM_SEED=42
-export DATA_DIR=data/ml-latest
-export OUTPUT_DIR=output
-export MODEL_DIR=output/model
 
 echo "Environment all set! Feel free to use notebook.ipynb to reproduce the results."
-
-jupyter notebook
