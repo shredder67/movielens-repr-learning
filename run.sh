35f364bc29cfe730f8cc6b06b4e195301fd02bb7
shredder67 <seriy.karp2@gmail.com>
2023-06-05 01:17:35 +0300

    some mini changes, setting up wsl

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ if [ ! -d "env" ]; then
     python3 -m venv env
     source env/bin/activate
     pip install -r requirements.txt
+    ipython kernel install --user --name=localenv
 else
     source env/bin/activate # all dependencies assumed to be installed
 fi
@@ -13,19 +14,26 @@ echo "Local environment set up!"
 
 # check for data directory, if not present, download and unpack movielens dataset
 
-if [ ! -d "data" ]; then
+if [ ! -d data ]; then
     echo "Downloading and unpacking dSata..."
     mkdir data
-    cd data
-    wget https://files.grouplens.org/datasets/movielens/ml-latest.zip
-    unzip ml-latest.zip
-    cd ..
 fi
 
+cd data
+
+if [ ! -f ml-latest.zip ]; then
+    wget https://files.grouplens.org/datasets/movielens/ml-latest.zip
+fi
+
+if [ ! -d raw ]; then
+    unzip ml-latest.zip -d ./raw
+fi
+
+cd ..
 echo "Data ready!"
 
 # set up directory for logging results and saving models
-if [ ! -d "output" ]; then
+if [ ! -d output ]; then
     mkdir output
     mkdir output/model
 fi
